952dbeb0edab501161c4e1e9d89a5eabc42dcbe2
ahmedkrdzalic <ahmed.krdzalic@zendev.se>
2023-06-27 02:06:05 +0200

    cookie sign part

--- src/routes/Sign.js
+++ src/routes/Sign.js
@@ -62,7 +62,8 @@ router.post("/login", async (req, res) => {
       return res
         .cookie("token", accessToken, {
           maxAge: 2592000000,
-          sameSite: "Lax",
+          sameSite: "None",
+          secure: true,
         })
         .json({ user: { email: user.email, _id: user._id } });
     }
